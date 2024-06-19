d38e82474c49807812ee8502133755dddf456665
myunghun choi <myunghun0721@gmail.com>
2024-06-19 13:59:22 -0700

    init for portfolio

--- src/index.jsx
+++ src/index.jsx
@@ -5,35 +5,15 @@ import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-const created = () => {
-    console.log('created')
-}
-
 root.render(
     <Canvas
-        camera={{
+        camera={ {
             fov: 45,
             near: 0.1,
-            far: 200,
-            position: [4, 2, 6]
-        }}
+            far: 2000,
+            position: [ -3, 1.5, 4 ]
+        } }
     >
         <Experience />
     </Canvas>
 )
-
-// root.render(
-//     <Canvas
-//         flat
-//         shadows
-//         camera={{
-//             fov: 45,
-//             near: 0.1,
-//             far: 200,
-//             position: [- 4, 6, 10]
-//         }}
-//         onCreated={created}
-//     >
-//         <Experience />
-//     </Canvas>
-// )
